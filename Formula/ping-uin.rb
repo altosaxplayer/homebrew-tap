841e8a2d31268519303d67b44a0e4b2f9f801eb6
Logan Childress <loganchildress@Mac.lan>
2026-08-30 09:04:41 -0400

    ping-uin: fix audit warnings

--- Formula/ping-uin.rb
+++ Formula/ping-uin.rb
@@ -1,5 +1,5 @@
 class PingUin < Formula
-  desc "btop-style TUI for monitoring IPs and hostnames"
+  desc "Btop-style TUI for monitoring IPs and hostnames"
   homepage "https://github.com/altosaxplayer/ping-uin"
   url "https://github.com/altosaxplayer/ping-uin/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "4744cabec62d07d7e6c2008af40db98a063746a65fd9ab81738d1e00ae182f4e"
@@ -12,7 +12,7 @@ class PingUin < Formula
   end
 
   test do
-    assert_predicate bin/"ping-uin", :exist?
+    assert_path_exists bin/"ping-uin"
     assert_predicate bin/"ping-uin", :executable?
   end
 end
